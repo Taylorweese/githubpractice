22659caa224c46ea85c402b8a603c3de7175b966
Taylor Weese <Taylor.weese1@gmail.com>
2018-05-24 13:34:46 +0000

    first commit

---
+++ height_to_centimeters.js
@@ -0,0 +1,9 @@
+
+var exclamation = prompt("say something whacky");
+
+var goodbye = prompt("how do you say bye?");
+
+var favoriteFood = prompt("whats your fav food?");
+
+
+console.log(exclamation + " " + favoriteFood + " is also my favorite food."  + goodbye);
